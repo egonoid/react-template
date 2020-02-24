3a44c2a6afeed0bcc4fa82ee1c8412f614ed98a0
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-02-24 10:48:12 +0100

    Improved unit tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders headline', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/egonoid React Template/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render headline', () => {
+    // Arrange
+
+    // Act
+    const { getByText } = render(<App />);
+
+    // Assert
+    const linkElement = getByText(/egonoid React Template/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    // Arrange
+
+    // Act
+    const container = render(<App />);
+
+    // Assert
+    expect(container).toMatchSnapshot();
+  });
 });
